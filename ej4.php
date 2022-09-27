858ee151f76b08ef521a90bc6b0c9a0fec4e216a
mglora <10695@cifpceuta.es>
2022-09-27 09:55:14 +0200

    exercises class 27/09

--- ej4.php
+++ ej4.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TABLA</title>
+    <style>
+        table {
+            width: 300px;
+            height: 300px;
+        }
+    </style>
 </head>
 
 <body>
@@ -16,7 +22,6 @@
     <?php
     /*
     
-        A partir de un número de filas y columnas, crear una tabla con ese tamaño. Las celdas deben estar rellenadas con los valores de las coordenadas de cada celda. (tabla.php)
     Basándote en el ejercicio anterior, rellena la tabla de manera que solo los bordes tengan contenido, quedándose el resto de celdas en blanco. (cuadrado.php)
     Basándote en el ejercicio anterior, ahora sólo debe aparecer el contenido de los dos diagonales.(equis.php)
     
@@ -25,7 +30,7 @@
 
     if (!isset($_GET['cantidad'])) {
     ?>
-        <form method="get" action="ej3.php">
+        <form method="get" action="#">
             <input type="number" name="cantidad">
             <input type="submit" name="enviar" value="Enviar">
         </form>
@@ -38,7 +43,11 @@
         for ($i = 0; $i < $cantidad; $i++) {
             echo "<tr>";
             for ($j = 0; $j < $cantidad; $j++) {
-                echo "<td>$i,$j</td>";
+                if ($i == 0 || $j == 0 || $i == $cantidad - 1 || $j == $cantidad - 1) {
+                    echo "<td>X</td>";
+                } else {
+                    echo "<td></td>";
+                }
             }
             "</tr>";
         }
---
+++ ej6.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TABLA</title>
+    <style>
+        table {
+            width: 50%;
+            margin: auto;
+        }
+
+        td {
+            height: 50px;
+            width: 50px;
+            text-align: center;
+        }
+
+        th {
+            background-color: blue;
+            color: azure;
+            height: 50px;
+            width: 50px;
+            text-align: center;
+        }
+
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Ejercicios con tabla de multiplicar</h1>
+
+    <?php
+    /*
+    Crea un programa que muestre por pantalla un cuadrado exactamente igual (fíjate bien en los encabezados, tanto de las filas como de las columnas) al de la imagen con las tablas de multiplicar.
+    */
+
+    const filcol = 11;
+    echo "<table border='1'>";
+
+    for ($i = -1; $i < filcol; $i++) {
+        echo "<tr>";
+        for ($j = -1; $j < filcol; $j++) {
+            if ($j == -1 && $i == -1) {
+                echo "<th>X</th>";
+            } elseif ($j == -1) {
+                echo "<td style='background-color: orange; color:azure;'>$i</td>";
+            } elseif ($i == -1) {
+                echo "<th>$j</th>";
+            } else {
+                echo "<td>", ($i) * ($j), "</td>";
+            }
+        }
+        "</tr>";
+    }
+    echo "</table>";
+    ?>
+</body>
+
+</html>
