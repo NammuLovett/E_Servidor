a67d3e93bb597484817f52573d3531e23102d676
nammulovett <nammmu@gmail.com>
2022-10-02 11:21:23 +0200

    changes

--- ej2.php
+++ ej2.php
@@ -13,16 +13,17 @@
     <h1>Ejercicios Bucles</h1>
 
     <?php
-    if (isset($_GET['cantidad'])) {
+    if (isset($_GET['cantidad'])) { //Si la variable cantidad está vacío
         $cantidad = $_GET['cantidad'];
         echo "<h2>Hay que hacer $cantidad input</h2>";
         echo "<form method='get' action='#'>";
         echo "<input type='hidden' name='cantidad' value='$cantidad'>";
-        for ($i = 1; $i <= $cantidad; $i++) {
+        for ($i = 1; $i <= $cantidad; $i++) { //Bucle para generar inputs
             echo "<input type='number' name='N$i'><br>";
         }
         echo "<input type= 'submit' name='Sumar' value='Sumar'>";
         echo "</form>";
+        //Suma
         if (isset($_GET['Sumar'])) {
             $suma = 0;
             for ($j = 1; $j <= $cantidad; $j++) {
@@ -33,7 +34,7 @@
         }
     } else {
     ?>
-        <form method="get" action="ej2.php">
+        <form method="get" action="#">
             <input type="number" name="cantidad">
             <input type="submit" name="enviar" value="Enviar">
         </form>
