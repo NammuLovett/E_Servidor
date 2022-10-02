ca01b5e802113be2d4ca68d5c55636325fa71241
nammulovett <nammmu@gmail.com>
2022-10-02 10:51:30 +0200

    add #

--- ej2.php
+++ ej2.php
@@ -9,39 +9,37 @@
 </head>
 
 <body>
-    
+
     <h1>Ejercicios Bucles</h1>
 
     <?php
-    if (isset($_GET['cantidad']))
-    {
+    if (isset($_GET['cantidad'])) {
         $cantidad = $_GET['cantidad'];
         echo "<h2>Hay que hacer $cantidad input</h2>";
-        echo "<form method='get' action='ej2.php'>";
+        echo "<form method='get' action='#'>";
         echo "<input type='hidden' name='cantidad' value='$cantidad'>";
-        for ($i=1;$i<=$cantidad;$i++)
-        {
+        for ($i = 1; $i <= $cantidad; $i++) {
             echo "<input type='number' name='N$i'><br>";
         }
         echo "<input type= 'submit' name='Sumar' value='Sumar'>";
         echo "</form>";
-        if(isset($_GET['Sumar']))
-        {
+        if (isset($_GET['Sumar'])) {
             $suma = 0;
-            for ($j=1; $j<=$cantidad ; $j++) { 
-                $n = 'N' .$j;
+            for ($j = 1; $j <= $cantidad; $j++) {
+                $n = 'N' . $j;
                 $suma = $suma + $_GET[$n];
             }
             echo "La suma es $suma";
         }
-    }else{
-        ?>
+    } else {
+    ?>
         <form method="get" action="ej2.php">
-        <input type="number" name="cantidad">
-        <input type="submit" name="enviar" value="Enviar">
+            <input type="number" name="cantidad">
+            <input type="submit" name="enviar" value="Enviar">
         </form>
-        <?php
+    <?php
     }
     ?>
 </body>
+
 </html>
