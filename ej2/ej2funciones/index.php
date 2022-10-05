f1fa8c3142fcfe1e1a88ffefa3b0fbec60fca4b9
nammulovett <nammmu@gmail.com>
2022-10-05 17:55:10 +0200

    comments

--- ej2/ej2funciones/index.php
+++ ej2/ej2funciones/index.php
@@ -2,15 +2,15 @@
 
 include_once "funciones.php";
 
-if (isset($_GET['cantidad'])) {
-    $cantidad = $_GET['cantidad'];
+if (isset($_GET['cantidad'])) { //Si hay algo en cantidad entra 
+    $cantidad = $_GET['cantidad']; //Guardamos la variable fuera para los dos casos de uso
 
-    if (isset($_GET['sumar'])) {
+    if (isset($_GET['sumar'])) { //Si hay datos en la variable suma, llama a la función de suma
         suma($cantidad);
-    } else {
+    } else { //Si no hay datos en la variable suma, mete los inputs con n cantidad veces
         crearFormulario($cantidad);
     }
-} else { //primer formulario si no hay datos, recojo cantidad
+} else { // Si no hay nada en la primera pantalla, primer formulario si no hay datos, recojo cantidad
     echo '<form action="#" method="get">
     <p><label for="variables">Determina cuántas inputs quieres sumar</label>
     <p>  <input type="number" name="cantidad" />
