cbf34130f636a3b3b858c42b6c9838038f7413e3
NammuLovett <nammmu@gmail.com>
2022-11-10 14:11:04 +0100

    3

--- t1/bdAlumnos/inserts/insertAlum.php
+++ t1/bdAlumnos/inserts/insertAlum.php
@@ -1,14 +1,22 @@
 <?php
+include_once("../bdconnect.php");
 
+$cantidad = $_GET['cantidad'];
+for ($i = 0; $i < $cantidad; $i++) {
+    $id_group = 1;
+    $name = $_GET['name' . $i];
+    $surname = $_GET['surname' . $i];
+    $expedient = $_GET['expedient' . $i];
+    $tlfAlum = $_GET['tlfAlum' . $i];
+    $email = $_GET['email' . $i];
 
-
-$sql = "INSERT INTO alumno (nameAlum, surnameAlum, expedient, emailAlum)
-VALUES ('john','doe','12345','user@email.com')";
-
-if ($conn->query($sql) === true) {
-    echo "Registro realizado con éxito";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    $sql = "INSERT INTO alumno
+VALUES (null, $id_group,'$name','$surname',$expedient,'$tlfAlum','$email')";
+    $conn->query($sql);
 }
+
+
+echo "Enviado con éxito";
+
 
 $conn->close();
