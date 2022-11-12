bf02aaeabb0c0b0f4ae6a186d3ca31e3f2da21ba
nammulovett <nammmu@gmail.com>
2022-11-12 19:52:24 +0100

    insert tut

---
+++ t1/bdAlumnos/forms/formTut.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <!-- charset  unicode, todos los idiomas, emojis... -->
+    <meta name="description" content="This is my first webpage ... " />
+    <!-- en name pones el nombre del metadato. y en content el metadato: En desctiption es una descripción en lenguaje natural  -->
+    <meta name="keywords" content="coding, html, programming" />
+    <!-- keywords ayuda a posicionarnos frente a los motores de búsqueda -->
+    <meta name="author" content="Manuel Gómez Lora" />
+    <!-- Autor de la página web -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Para dar soporte a las versiones antiguas de IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- El conjunto de cosas que se muestra en pantalla, es decir si no se pone, las páginas responsives no se muestran, es la referencia de estas-->
+    <meta name="generator" content="Programa" />
+    <!-- Si nuestra página web ha sido creada de forma automáticamente con un programa -->
+    <link rel="stylesheet" href="../style.css" />
+    <!-- Señalar contenido que tiene relación con el html de la web, tienes que poner en rel la palabra cable -->
+    <link rel="icon" href="img/favicon.ico" />
+    <!-- Favicon de la web -->
+    <title>BD MURALLA</title>
+</head>
+
+<body>
+    <form action="../inserts/insertTut.php" method="POST">
+        <fieldset>
+            <legend>Crear Tutorías</legend>
+
+            <select name="id_grupo">
+                <option value="" selected disabled hidden>-- Selecciona un grupo --</option>
+                <?php
+                include_once('../bdconnect.php');
+                $sql = "SELECT * FROM Grupo";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($grupo = $result->fetch_assoc()) {
+                        echo "<option value='" . $grupo['id_grupo'] . "'>" . $grupo['nombreGrupo'] . "</option>";
+                    }
+                }
+                ?>
+                </p>
+                <p>
+            </select>
+            <select name="id_profesor">
+                <option value="" selected disabled hidden>-- Selecciona un Profesor --</option>
+                <?php
+                include_once('../bdconnect.php');
+                $sql = "SELECT * FROM Profesor";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($prof = $result->fetch_assoc()) {
+                        echo "<option value='" . $prof['id_profesor'] . "'>" . $prof['emailP'] . "</option>";
+                    }
+                }
+                ?>
+                </p>
+                <p>
+            </select>
+
+            <input type="submit" name="insertar" value="Guardar">
+
+
+
+
+            </p>
+        </fieldset>
+</body>
+
+</html>
---
+++ t1/bdAlumnos/inserts/insertTut.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("../bdconnect.php");
+
+$idGrupo = $_POST['id_grupo'];
+$idProf = $_POST['id_profesor'];
+
+$sql = "INSERT INTO Tutoria (`id_grupo`, `id_profesor`)
+VALUES ('$idGrupo','$idProf')";
+
+if ($conn->query($sql) === true) {
+    echo "Registro realizado con éxito";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
