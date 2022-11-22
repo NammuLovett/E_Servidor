ca754517abef578d3dbfd5a06483efd2f887dee1
NammuLovett <nammmu@gmail.com>
2022-11-22 09:28:38 +0100

    php

--- t1/videoclub/soporte.php
+++ t1/videoclub/soporte.php
@@ -9,32 +9,43 @@ trabaje nuestro videoclub (cintas de vídeo, videojuegos, etc...):
     Crear un archivo ( inicio.php ) para usar las clases y copia el fragmento adjunto. -->
 <?php
 
+// propiedades 
+// constructor 
+// getters - setters 
+// resto de métodos 
 
 class Soportes
 {
     //propiedades
     public string $titulo; //pública + 
     protected int $numero; //protected #
-    private float $IVA;
-    private int $precio; //privada - 
+    private float $precio; //privada - 
+    private const IVA = 0.79;
 
-
+    public function __construct(string $titulo, int $numero, float $precio)
+    {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+    }
     //métodos 
     public function getPrecio()
     {
+        return "El precio es " . $this->getPrecio() . " euros";
     }
 
     public function getPrecioConIva()
     {
+        return $this->getPrecio() * self::IVA . " euros";
     }
 
     public function getNumero()
     {
+        return $this->numero;
     }
 
     public function muestraResumen()
     {
+        echo $this->titulo . "</br>" . $this->precio . " € (IVA no incluido)";
     }
 }
-
-?>
