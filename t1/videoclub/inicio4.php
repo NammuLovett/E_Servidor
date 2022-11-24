9627ac9d10520380ec28be315ab59c3bf1fe4699
nammulovett <nammmu@gmail.com>
2022-11-24 17:14:32 +0100

    p

--- t1/videoclub/inicio4.php
+++ t1/videoclub/inicio4.php
@@ -3,7 +3,7 @@
 include "soporte.php";
 include "juego.php";
 
-$miJuego = new Juego("The Last of Us II", 26, 49.99, "PS4", 1, 1);
+$miJuego = new Juego("The Last of Us II", 26, 49.99, "PS4", 1, 2);
 echo "<strong>" . $miJuego->titulo . "</strong>";
 echo "<br>Precio: " . $miJuego->getPrecio() . " euros";
 $miJuego->muestraResumen();
