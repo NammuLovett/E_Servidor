db8a2528ef60ad8eda32d5e7aa1454eee1f71913
nammulovett <nammmu@gmail.com>
2022-11-24 22:30:36 +0100

    .

--- t1/videoclub/cliente.php
+++ t1/videoclub/cliente.php
@@ -1,63 +1,157 @@
 <?php
+include_once 'soporte.php';
 
-/* Crear la clase Cliente. El constructor recibirá el nombre,numero y maxAlquilerConcurrente, este último pudiendo ser opcional y tomando como valor por defecto 3. 
-
-Tras ello, añade getter/setter  únicamente a numero, y un getter a numSoportesAlquilados (este campo va a almacenar un contador del total de alquileres que ha realizado). El array de soportes alquilados contedrá clases que hereden de Soporte. Finalmente, añade el método muestraResumen que muestre el nombre y la cantidad de alquileres (tamaño de Array soportesAlquilados). */
-
-/* declare(strict_types=1); */
+/* declare(strict_types=1); ver documentación*/ 
 
 class Cliente
 {
+    public string $nombre;
+    private int $numero;
+    private int $maxAlquilerConcurrente;
+    private int $numSoportesAlquilados;
+    private array $soportesAlquilados;
 
-    public  $nombre; //publica
-    private  $numero; //priv
-    private  $numSoportesAlquilados; //priv
-    private  $soportesAlquilados = array(); //priv
-    private  $maxAlquilerConcurrente; //priv
-
-
-    public   function __construct(string $nombre, int $numero, array $soportesAlquilados, int $maxAlquilerConcurrente)
+    public function __construct(string $nombre, int $numero, int $maxAlquilerConcurrente = 3)
     {
+        $this->nombre = $nombre;
+        $this->numero = $numero;
+        $this->maxAlquilerConcurrente = $maxAlquilerConcurrente;
     }
+
+    public function getNumero() //getNumero
+    {
+        return $this->numero;
+    }
+    public function setNumero($numero) //setNum
+    {
+        $this->numero = $numero;
+    }
+    public function getNumSoportesAlquilados()
+    {
+        return $this->numSoportesAlquilados;
+    }
+    public function muestraResumen()
+    {
+        echo 'Nombre: ' . $this->nombre . '</br>Cantidad de alquileres: ' . count($this->soportesAlquilados) . '<br>';
+    }
+
+    /* cliente 1 */
+
+
+
 }
 
 
 
 
+
+
+El array de soportes alquilados contedrá clases que hereden de Soporte. 
+
+
+
 /* 
-class Soporte
+class Cliente
 {
-    //propiedades
-    public $titulo; //pública + 
-    protected $numero; //protected #
-    private $precio; //privada - 
-    private const IVA = 1.21;
 
-    public function __construct(string $titulo, int $numero, float $precio)
-    {
-        $this->titulo = $titulo;
-        $this->numero = $numero;
-        $this->precio = $precio;
-    }
-    //métodos 
-    public function getPrecio()
-    {
-        return $this->precio;
-    }
+    public string $nombre;
+    private int $numero;
+    private static int $totalClientes = 0;
+    private array $soportesAlquilados = array();
+    private int $numSoportesAlquilados = 0;
+    private int $maxAlquilerConcurrente;
 
-    public function getPrecioConIva()
+
+    public function __construct(string $nombre, int $maxAlquilerConcurrente = 3)
     {
-        return $this->getPrecio() * self::IVA;
+        $this->nombre = $nombre;
+        $this->maxAlquilerConcurrente = $maxAlquilerConcurrente;
+        $this->numero = self::$totalClientes;
+        self::$totalClientes++;
     }
 
     public function getNumero()
     {
         return $this->numero;
     }
+    public function setNumero(int $numero)
+    {
+        $this->numero = $numero;
+    }
+
+    public function getNumSoportesAlquilados()
+    {
+        return $this->numSoportesAlquilados;
+    }
+
+    public function tieneAlquilado(Soporte $s): bool
+    {
+        if (in_array($s, $this->soportesAlquilados)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function alquilar(Soporte $s): bool
+    {
+        if ($this->tieneAlquilado($s)) {
+            echo 'Ya tienes este soporte alquilado.<br>';
+            return false;
+        } else {
+            if ($this->getNumSoportesAlquilados() < $this->maxAlquilerConcurrente) {
+                array_push($this->soportesAlquilados, $s);
+                $this->numSoportesAlquilados++;
+                $s->setAlquilado(true);
+                echo 'Soporte alquilado correctamente.<br>';
+                return true;
+            } else {
+                echo 'No se puede alquilar. Número máximo de soportes alcanzado (' . $this->maxAlquilerConcurrente . ').<br>';
+                return false;
+            }
+        }
+    }
+
+    public function devolver(int $numSoporte): bool
+    {
+        if ($this->numSoportesAlquilados == 0) {
+            echo 'No tiene ningún soporte alquilado. <br>';
+            return false;
+        } else {
+            for ($i = 0; $i < $this->numSoportesAlquilados - 1; $i++) {
+                if ($this->soportesAlquilados[$i]->getNumero() == $numSoporte) {
+                    $Soporte = $this->soportesAlquilados[$i];
+                    $this->soportesAlquilados[$i] = $this->soportesAlquilados[$i + 1];
+                    $this->soportesAlquilados[$i + 1] = $Soporte;
+                }
+            }
+
+            if ($this->soportesAlquilados[count($this->soportesAlquilados) - 1]->getNumero() == $numSoporte) {
+                $this->soportesAlquilados[count($this->soportesAlquilados) - 1]->setAlquilado(false);
+                array_pop($this->soportesAlquilados);
+                echo 'Soporte devuelto correctamente. <br>';
+                $this->numSoportesAlquilados--;
+                return true;
+            } else {
+                echo 'No tiene alquilado un soporte con ese número. <br>';
+                return false;
+            }
+        }
+    }
+
+    public function listarAlquileres(): void
+    {
+        echo '<strong>' . $this->nombre . ' tiene ' . count($this->soportesAlquilados) . ' soportes alquilados: <br></strong>';
+        foreach ($this->soportesAlquilados as $Soporte) {
+            echo '<br>';
+            echo get_class($Soporte) . ':<br>';
+            echo $Soporte->muestraResumen();
+        }
+    }
 
     public function muestraResumen()
     {
-        echo "<br> " . $this->titulo . " " . $this->precio . " € (IVA no incluido)";
+        echo 'Nombre: ' . $this->nombre . '</br>Cantidad de alquileres: ' . count($this->soportesAlquilados) . '<br>';
     }
 }
  */
