58cfeb0d842075dba7770bff2fc49be957dcaa21
NammuLovett <nammmu@gmail.com>
2022-12-12 14:10:20 +0100

    fantasi

--- t2/mvc/index.php
+++ t2/mvc/index.php
@@ -8,13 +8,17 @@ require_once 'controllers/note.php';
 
 if (!isset($_GET["action"])) $_GET["action"] = constant("DEFAULT_ACTION");
 
+
 $controlador = new noteController();
 
 $dataToView = array();
 $dataToView = $controlador->{$_GET["action"]()};
 
 
-/*LOAD views*/
+/*LOAD views
+
+https://code.tutsplus.com/tutorials/create-a-sticky-note-effect-in-5-easy-steps-with-css3-and-html5--net-13934
+*/
 
 require_once 'view/template/header.php';
 require_once 'view/' . $controlador->view . '.php';
