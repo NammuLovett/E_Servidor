6580fa6b31fb39b4a665613eb81ba2e85cc65d2a
NammuLovett <nammmu@gmail.com>
2022-12-12 13:16:42 +0100

    .

--- t2/mvc/index.php
+++ t2/mvc/index.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+$accion = $GET['action'];
+$datos = array();
+$cont = new noteControler;
+$datos = $cont->getDatos($accion);
 
-<body>
-</body>
-
-</html>
+require_once 'header.php';
+require_once '$cont->vista.php';
+require_once 'footer.php';
