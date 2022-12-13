5bfe709ef626e074e8d9ccace9ae6db1ac417971
nammulovett <nammmu@gmail.com>
2022-12-13 08:50:32 +0100

    .

--- t2/mvc/Controler/note.php
+++ t2/mvc/Controler/note.php
@@ -13,19 +13,22 @@ class noteController
         $this->page_title = '';
         $this->noteObt = new NoteTable();
     }
-}
 
-/*List all notes*/
-public function list(){
-    $this->page_title = 'Listado de Notas';
-    return $this->noteObt->getNotes();
-}
-public function insert(){
-    $this->page_title = 'Insertar Nota';
-    $this->view = 'insert_notes';
 
-}
+    /*List all notes*/
+    public function list()
+    {
+        $this->page_title = 'Listado de Notas';
+        return $this->noteObt->getNotes();
+    }
+    public function insert()
+    {
+        $this->page_title = 'Insertar Nota';
+        $this->view = 'insert_notes';
+    }
 
-public function guardar(){
-$this->view = 'list_notes';
+    public function guardar()
+    {
+        $this->view = 'list_notes';
+    }
 }
