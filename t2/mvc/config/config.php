270e7c7190598ac4d095d04aec2d497d77e1c14c
nammulovett <nammmu@gmail.com>
2022-12-13 09:34:29 +0100

    bdcon

--- t2/mvc/config/config.php
+++ t2/mvc/config/config.php
@@ -1,3 +1,10 @@
-<?php 
+<?php
 
 /*DATABASE VALUE */
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'mvc_example');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
+/*DEFAULT*/
+define('DEFAULT_ACTION', 'list');
