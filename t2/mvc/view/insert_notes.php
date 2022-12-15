d177120397e02d3354767ad71643d79e3ffea65b
nammulovett <nammmu@gmail.com>
2022-12-15 19:03:26 +0100

    mes

--- t2/mvc/view/insert_notes.php
+++ t2/mvc/view/insert_notes.php
@@ -2,13 +2,17 @@
     <!-- Formulario que envía por post los datos de la nueva nota al index con el action insert -->
     <form action="index.php?action=list&insNotes=true" method="post">
 
-        <div class="insertNote">
-            <input class="title" type="text" name="title" placeholder="Título..." />
-            <hr />
-            <textarea class="content" name="content" placeholder="Contenido..."></textarea>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">Título</span>
+            <input class="form-control" type="text" name="title" placeholder="Escriba aquí..." required></input>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">Texto</span>
+            <input class="form-control" name="content" placeholder="Escriba aquí..." required></input>
         </div>
 
-        <input class="ok" type="submit" value="Aceptar" />
+
+        <input class="btn btn-primary" type="submit" value="Aceptar" />
+        <a href="index.php"><button class="btn btn-outline-danger">Cancelar</button></a> <!-- Botón de cancelar -->
     </form>
-    <a href="index.php"><button class="cancel">Cancelar</button></a> <!-- Botón de cancelar -->
 </div>
